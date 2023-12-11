42cf5dd8ef082af61fb8d2ed5e5f0d70064dcdc5
Jay Shi <25982097@qq.com>
2023-12-12 00:39:59 +0800

    Refractor CreateTaskCommand

--- Application/Commands/CreateTaskCommand.cs
+++ Application/Commands/CreateTaskCommand.cs
@@ -9,19 +9,12 @@ namespace Application.Commands
     {
         public CreateTaskCommand(CreateTaskRequest request)
         {
-            Task = new TaskEntity
-            {
-                Title = request.Title,
-                Description = request.Description,
-                OwnerId = Guid.Parse(request.OwnerId),
-                StartedOn = request.StartedOn,
-                Period = request.Period,
-                Status = RecordStatus.Initial,
-                ModifiedOn = DateTime.UtcNow,
-                ModifiedById = Guid.Parse(request.OwnerId),
-                CreatedOn = DateTime.UtcNow,
-                CreatedById = Guid.Parse(request.OwnerId)
-            };
+            Task = TaskEntity.Create(
+                request.Title,
+                request.Description,
+                Guid.Parse(request.OwnerId),
+                request.Period,
+                Guid.Parse(request.OwnerId));
         }
 
         public TaskEntity Task { get; private set; }
